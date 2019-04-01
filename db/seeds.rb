4090e2df252a6d1353a7a5c2a703fa1eba23daea
akatsuka <akatsukasan1900@gmail.com>
2019-04-01 17:50:02 +0900

    commit8

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Question.create(id: 1, name: 'Test name 1', title: 'Test title 1', content: 'Test content 1')
-Question.create(id: 2, name: 'Test name 2', title: 'Test title 2', content: 'Test content 2')
-Question.create(id: 3, name: 'Test name 3', title: 'Test title 3', content: 'Test content 3')
+# Question.create(id: 1, name: 'Test name 1', title: 'Test title 1', content: 'Test content 1')
+# Question.create(id: 2, name: 'Test name 2', title: 'Test title 2', content: 'Test content 2')
+# Question.create(id: 3, name: 'Test name 3', title: 'Test title 3', content: 'Test content 3')
