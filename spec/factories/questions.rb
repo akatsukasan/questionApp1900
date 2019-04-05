a08233452dfe0bda7ca76f1a2a1ca5f43af06117
akatsuka.account <47320492+akatsukasan@users.noreply.github.com>
2019-04-05 10:35:51 +0900

    Update questions.rb

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -4,9 +4,4 @@ FactoryBot.define do
     sequence(:title) { |n| "TEST#{n}TITLE"}
     sequence(:content) { |n| "TEST_CONTE#{n}"}
   end
-  factory :invalid_question, class: Question do
-    sequence(:name) { |n| "TEST_NAME#{n}"}
-    sequence(:title) { |n| "TEST#{n}TITLE"}
-    sequence(:content) { |n| "TEST_CONTE#{n}"}
-  end
 end
